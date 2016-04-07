2f3ed639b35e7135a72211af02cff92dca988104
Michael Hale <mj504b@gmail.com>
2016-04-07 12:07:12 -0400

    Use chi-squared statistic test to find deciphered message.

--- set1/challenge3.py
+++ set1/challenge3.py
@@ -18,49 +18,62 @@ __author__ = 'michael@michaelhale.org (Michael Hale)'
 from challenge1 import HEX_LUT, hex_to_bytes
 from challenge2 import bytes_to_hex, hex_xor
 from collections import defaultdict
+from pprint import pprint
 
-# Normalized to have a total probability of ~1
+# Normalized frequency table which has a total probability of ~1
 LETTER_FREQUENCY = {
-    'A': .0812,
-    'B': .0149,
-    'C': .0271,
-    'D': .0432,
-    'E': .1202,
-    'F': .0230,
-    'G': .0203,
-    'H': .0592,
-    'I': .0731,
-    'J': .0010,
-    'K': .0069,
-    'L': .0398,
-    'M': .0261,
-    'N': .0695,
-    'O': .0768,
-    'P': .0182,
-    'Q': .0011,
-    'R': .0602,
-    'S': .0628,
-    'T': .0910,
-    'U': .0288,
-    'V': .0111,
-    'W': .0209,
-    'X': .0017,
-    'Y': .0211,
-    'Z': .0007
+    'a': .0812,
+    'b': .0149,
+    'c': .0271,
+    'd': .0432,
+    'e': .1202,
+    'f': .0230,
+    'g': .0203,
+    'h': .0592,
+    'i': .0731,
+    'j': .0010,
+    'k': .0069,
+    'l': .0398,
+    'm': .0261,
+    'n': .0695,
+    'o': .0768,
+    'p': .0182,
+    'q': .0011,
+    'r': .0602,
+    's': .0628,
+    't': .0910,
+    'u': .0288,
+    'v': .0111,
+    'w': .0209,
+    'x': .0017,
+    'y': .0211,
+    'z': .0007
     }
 
 def hex_decode(hex):
+    """Converts hex string to ASCII string."""
     plain = ''
     for b in hex_to_bytes(hex):
         plain += chr(b)
     return plain
 
 def decrypt_message(hex):
-    message = ""
+    """Decrypts a single-byte XOR cipher using a chi-squared test."""
     candidates = defaultdict(list)
+    # Iterate through each possible key
     for i in range(256):
         candidate_hex = hex_xor(hex, bytes_to_hex(bytes([i] * len(hex))))
         candidate_plain = hex_decode(candidate_hex)
+        score = 0.0
+        # Iterate through every letter of the english alphabet
+        for c in range(ord('a'), ord('z')+1):
+            c = chr(c)
+            c_count = candidate_plain.lower().count(c)
+            c_expected = LETTER_FREQUENCY[c] * len(candidate_hex)
+            # Chi-squared calculation
+            score += ((c_count - c_expected)**2) / c_expected
+        candidates[i].append([score, candidate_hex, candidate_plain])
+    message = min(candidates.items(), key=lambda a: a[1])
     return message
 
 def main():
