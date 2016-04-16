0e3820af84f479cab56ea476c12ebf81589e6c94
Michael Hale <mj504b@gmail.com>
2016-04-16 10:41:37 -0400

    Move instructions to Markdown file.

--- set1/challenge6.py
+++ set1/challenge6.py
@@ -6,28 +6,12 @@ There's a file here (challenge6.txt). It's been base64'd after being encrypted w
 
 Decrypt it.
 
-Here's how:
-
-1. Let KEYSIZE be the guessed length of the key; try values from 2 to (say) 40.
-
-2. Write a function to compute the edit distance/Hamming distance between two strings. The Hamming distance is just the number of differing bits.
-
-3. For each KEYSIZE, take the first KEYSIZE worth of bytes, and the second KEYSIZE worth of bytes, and find the edit distance between them. Normalize this result by dividing by KEYSIZE.
-
-4. The KEYSIZE with the smallest normalized edit distance is probably the key. You could proceed perhaps with the smallest 2-3 KEYSIZE values. Or take 4 KEYSIZE blocks instead of 2 and average the distances.
-
-5. Now that you probably know the KEYSIZE: break the ciphertext into blocks of KEYSIZE length.
-
-6. Now transpose the blocks: make a block that is the first byte of every block, and a block that is the second byte of every block, and so on.
-
-7. Solve each block as if it was single-character XOR. You already have code to do this.
-
-8. For each block, the single-byte XOR key that produces the best looking histogram is the repeating-key XOR key byte for that block. Put them together and you have the key.
-
-This code is going to turn out to be surprisingly useful later on. Breaking repeating-key XOR ("Vigenere") statistically is obviously an academic exercise, a "Crypto 101" thing. But more people "know how" to break it than can actually break it, and a similar technique breaks something much more important.
+@TODO Implement base64 decode.
 """
 
 __author__ = 'michael@michaelhale.org (Michael Hale)'
+
+from base64 import b64decode
 
 def dec_to_bin(string):
     """Returns the binary representation of a string"""
@@ -41,10 +25,18 @@ def hamming_distance(first, second):
     second = dec_to_bin(second)
     return sum(b0 != b1 for b0, b1 in zip(first, second))
 
+def decrypt_vigenere(cipher, keysize_min, keysize_max):
+    for key_length in range(keysize_min, keysize_max):
+        b0 = cipher[:key_length]
+        print(b0)
+    return None
+
 def main():
     FIRST = 'this is a test'
     SECOND = 'wokka wokka!!!'
-    print(hamming_distance(FIRST, SECOND))
+    in_file = open('challenge6.txt', 'r')
+    base64 = in_file.read()
+    decrypt_vigenere(b64decode(base64), 2, 40)
 
 if __name__ == '__main__':
     main()
